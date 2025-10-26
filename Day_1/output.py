0c9babd1bd65baee120c95c53b6e08f68c01e98f
Rahul311-R <rahul541r@gmail.com>
2025-10-26 12:47:49 +0530

    26/10/2025

---
+++ Day_1/output.py
@@ -0,0 +1,6 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+
+print(name)
+print(age)
+print(f"hello {name} is {age+1} years old.")
--- Day_1/print.py
+++ Day_1/print.py
@@ -1,1 +1,12 @@
-print("Hello World")
+# Arithmetic
+a = 10
+b = 5
+print(a + b)  # 15
+print(a - b)  # 5
+print(a * b)  # 50
+print(a / b)  # 2.0
+
+# String operations
+first_name = "Chief"
+last_name = "Coder"
+print(first_name + " " + last_name)  # Chief Coder

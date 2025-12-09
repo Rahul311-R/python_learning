2d93031200734454e05318c7351dc0141220c632
Rahul R <rahul541r@gmail.com>
2025-12-09 11:08:58 +0530

    09/12/2025

---
+++ Day_2/type cast.py
@@ -0,0 +1,5 @@
+x = int(input("Enter a number: "))
+print(x + x)
+
+h = float(input("Enter your height in cm: "))
+print("Height in meters =", h / 100)
---
+++ Day_2/user input.py
@@ -0,0 +1,6 @@
+name = input("Enter the name:")
+print("your name is", name)
+
+x = input("Enter a number:")
+print(x+x)
+

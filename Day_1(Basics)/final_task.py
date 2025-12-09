ce5c5dea0ed415a21e7ceb5b505bb53c748f144c
Rahul R <rahul541r@gmail.com>
2025-12-09 07:39:13 +0530

    09/12/2025

---
+++ Day_1(Basics)/final_task.py
@@ -0,0 +1,7 @@
+print("Python Day 1 completed!")
+
+your_name = "Rahul"
+your_age = 21
+favourite_colour = "Purple"
+
+print(your_name, your_age, favourite_colour)
---
+++ Day_1(Basics)/variable.py
@@ -0,0 +1,9 @@
+name = "Rahul"
+age = 21
+height = 5.9
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)

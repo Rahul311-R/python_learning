c5bdf0ffe1a9f313c3e5ffd0c0bf6fc1276cf67f
Rahul R <rahul541r@gmail.com>
2025-12-10 14:19:48 +0530

    10/12/2025

---
+++ Day_6/module1.py
@@ -0,0 +1,5 @@
+from math import sqrt
+print(int(sqrt(int(input()))))
+
+import math as m
+print(m.factorial(5))
---
+++ Day_6/module5.py
@@ -0,0 +1,6 @@
+import module4 as m
+a = int(input())
+b = int(input())
+
+print(m.add(a,b))
+print(m.mul(a,b))

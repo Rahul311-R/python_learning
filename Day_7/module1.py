7fa5ad3d0b8b6767a184fb5ab22d79cd62ca0b41
Rahul R <rahul541r@gmail.com>
2025-12-11 04:47:53 +0530

    11/12/2025

---
+++ Day_7/module1.py
@@ -0,0 +1,5 @@
+import math as m
+
+print(m.sqrt(25))
+print(m.pi)
+print(m.factorial(5))
---
+++ Day_7/module2.py
@@ -0,0 +1,4 @@
+import random
+
+print(random.randint(1, 100))  # random number
+print(random.choice(["apple", "banana", "mango"]))

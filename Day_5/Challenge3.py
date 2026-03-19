eb59450290074bc3326475fae32a4ee8ce00d737
Rahul311-R <rahul541r@gmail.com>
2026-03-19 09:05:45 +0000

    19/03/2026

--- Day_5/Challenge3.py
+++ Day_5/Challenge3.py
@@ -4,6 +4,6 @@ full_price = 500
 if age <12:
     print("Ticket is free!")
 elif age>60:
-    print(f"Ticket price: ₹{250}")
+    print(f"Ticket price: ₹{full_price / 2}")
 else:
-    print(f"Ticket price: ₹{500}")
+    print(f"Ticket price: ₹{full_price}")

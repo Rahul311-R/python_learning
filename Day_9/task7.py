21c3ecdf12dc1d3bf9f332dacc6d38be7db7a588
Rahul311-R <rahul541r@gmail.com>
2026-03-23 09:17:17 +0000

    23/03/2026

--- Day_9/task7.py
+++ Day_9/task7.py
@@ -1,13 +1,13 @@
 nums = [1, 5, 3, 4, 2, 8, 6]
 k = 2
 
-num_set = set(nums)   # convert to set for fast lookup
+num_set = set(nums)  
 
 pairs = []
 for num in nums:
-    if num+k in num_set:        # check if num+k exists
-        pairs.append((num,num+k))  # add pair in ascending order
+    if num+k in num_set:    
+        pairs.append((num,num+k)) 
 
-pairs.sort()   # sort pairs
+pairs.sort()   
 for pair in pairs:
     print(pair)
